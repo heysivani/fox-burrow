375e04b6cf385b77f956a38a3cab3d7bf0ae6b25
heysivani <heysivani@gmail.com>
2021-02-26 21:07:25 +0900

    completed demo code! yay~

--- src/FoxBurrow.vue
+++ src/FoxBurrow.vue
@@ -1,15 +1,22 @@
 <template>
-  <main class="">
-    <div id="card" class="">
-      <h1 class="">
+  <main
+    class="bg-yellow-900 sm:bg-foxy-brown flex justify-center items-center h-screen"
+  >
+    <div
+      id="card"
+      class="bg-yellow-500 flex flex-col items-center hover:bg-yellow-400 rounded-lg"
+    >
+      <h1
+        class="text-2xl font-mono text-yellow-900 my-2 capitalize tracking-widest"
+      >
         mr.fox
       </h1>
       <img
         src="https://randomfox.ca/images/22.jpg"
         alt="A beautiful fox"
-        class=""
+        class="w-5/6 border-8 border-yellow-700 rounded-lg"
       />
-      <p class="">
+      <p class="text-md font-mono text-yellow-900 my-2 italic leading-loose">
         "What does the fox say?" <br />
         "Hehehehehe"
       </p>
